f0d3588e1cd5018128ccd54c7c3be49cbe3b2d73
huwenhai <25935317@qq.com>
2025-10-27 14:22:24 +0800

    improve

--- src/behavior_tree/json_parser.rs
+++ src/behavior_tree/json_parser.rs
@@ -1,14 +1,68 @@
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use serde_json::from_str;
+use std::collections::HashMap;
 
-use super::interface::{IParser, TaskAddData,ITaskProxy};
+use super::interface::{IParser, TaskAddData,ITaskProxy, IAction, IConditional, IComposite, IDecorator, RealTaskType};
 use super::runtime::TaskProxy;
 
 pub struct JsonParser{
-
+    action_fn: HashMap<String, fn(variables:HashMap<String, serde_json::Value>,id_2_task:HashMap<u32, Weak<Box<dyn ITaskProxy>>>) -> Box<dyn IAction>>,
+    conditional_fn: HashMap<String, fn(variables:HashMap<String, serde_json::Value>,id_2_task:HashMap<u32, Weak<Box<dyn ITaskProxy>>>) -> Box<dyn IConditional>>,
+    composite_fn: HashMap<String, fn(variables:HashMap<String, serde_json::Value>,id_2_task:HashMap<u32, Weak<Box<dyn ITaskProxy>>>) -> Box<dyn IComposite>>,
+    decorator_fn: HashMap<String, fn(variables:HashMap<String, serde_json::Value>,id_2_task:HashMap<u32, Weak<Box<dyn ITaskProxy>>>) -> Box<dyn IDecorator>>,
 }
 
 impl JsonParser{
+    pub fn new() -> Self{
+        Self{
+            action_fn: HashMap::new(),
+            conditional_fn: HashMap::new(),
+            composite_fn: HashMap::new(),
+            decorator_fn: HashMap::new(),
+        }
+    }
+
+    pub fn register_action_fn(&mut self, name:&str, action_generate_fn:fn(variables:HashMap<String, serde_json::Value>,id_2_task:HashMap<u32, Weak<Box<dyn ITaskProxy>>>) -> Box<dyn IAction>){
+        self.action_fn.insert(name.to_string(), action_generate_fn);
+    }
+
+    pub fn register_conditional_fn(&mut self, name:&str, conditional_generate_fn:fn(variables:HashMap<String, serde_json::Value>,id_2_task:HashMap<u32, Weak<Box<dyn ITaskProxy>>>) -> Box<dyn IConditional>){
+        self.conditional_fn.insert(name.to_string(), conditional_generate_fn);
+    }
+
+    pub fn register_composite_fn(&mut self, name:&str, composite_generate_fn:fn(variables:HashMap<String, serde_json::Value>,id_2_task:HashMap<u32, Weak<Box<dyn ITaskProxy>>>) -> Box<dyn IComposite>){
+        self.composite_fn.insert(name.to_string(), composite_generate_fn);
+    }
+
+    pub fn register_decorator_fn(&mut self, name:&str, decorator_generate_fn:fn(variables:HashMap<String, serde_json::Value>,id_2_task:HashMap<u32, Weak<Box<dyn ITaskProxy>>>) -> Box<dyn IDecorator>){
+        self.decorator_fn.insert(name.to_string(), decorator_generate_fn);
+    }
+
+
+    fn generate_real_task(&self, corresponding_type:&str, variables:HashMap<String, serde_json::Value>,id_2_task:HashMap<u32, Weak<Box<dyn ITaskProxy>>>) -> Result<RealTaskType, Box<dyn std::error::Error>>{
+        if self.action_fn.contains_key(corresponding_type){
+            return Ok(RealTaskType::Action(self.action_fn.get(corresponding_type).unwrap()(variables, id_2_task)));
+        }
+        if self.conditional_fn.contains_key(corresponding_type){
+            return Ok(RealTaskType::Conditional(self.conditional_fn.get(corresponding_type).unwrap()(variables, id_2_task)));
+        }
+        if self.composite_fn.contains_key(corresponding_type){
+            return Ok(RealTaskType::Composite(self.composite_fn.get(corresponding_type).unwrap()(variables, id_2_task)));
+        }
+        if self.decorator_fn.contains_key(corresponding_type){
+            return Ok(RealTaskType::Decorator(self.decorator_fn.get(corresponding_type).unwrap()(variables, id_2_task)));
+        }
+
+        Err(format!("generate_real_task not implemented for corresponding_type: {}", corresponding_type).into())
+    }
+
+    fn generate_task_proxy(&self, task_json:&serde_json::Value, variables:HashMap<String, serde_json::Value>,id_2_task:HashMap<u32, Weak<Box<dyn ITaskProxy>>>) -> Result<Rc<Box<dyn ITaskProxy>>, Box<dyn std::error::Error>>{
+        let corresponding_type = task_json["Type"].as_str().unwrap();
+
+        Err("generate_task_proxy not implemented".into())
+    }
+
+
     fn initialize_task(&self, task_json:&serde_json::Value, task_add_data:&TaskAddData) -> Result<Rc<Box<dyn ITaskProxy>>, Box<dyn std::error::Error>>{
         let correspondingType = task_json["Type"].as_str().unwrap();
 
